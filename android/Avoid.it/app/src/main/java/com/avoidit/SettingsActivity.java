29fdbdcc40c72c899c5ee3a1ad55438a356df7d7
Nick Graves <ngraves95@gmail.com>
2016-10-02 20:35:31 -0400

    Working back button on settings

--- android/Avoid.it/app/src/main/java/com/avoidit/SettingsActivity.java
+++ android/Avoid.it/app/src/main/java/com/avoidit/SettingsActivity.java
@@ -133,6 +133,16 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            startActivity(new Intent(this, HomeActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -185,7 +195,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         public boolean onOptionsItemSelected(MenuItem item) {
             int id = item.getItemId();
             if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), SettingsActivity.class));
+                startActivity(new Intent(getActivity(), HomeActivity.class));
                 return true;
             }
             return super.onOptionsItemSelected(item);
