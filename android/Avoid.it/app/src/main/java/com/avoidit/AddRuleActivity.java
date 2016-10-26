a6b2b78f49c206e448cd39ddc9174cc405174a25
Anthon Holmqvist <anthon.kendel@gmail.com>
2016-10-26 17:16:11 -0400

    Changed layout again on add rule.

--- android/Avoid.it/app/src/main/java/com/avoidit/AddRuleActivity.java
+++ android/Avoid.it/app/src/main/java/com/avoidit/AddRuleActivity.java
@@ -11,10 +11,6 @@ import android.widget.Spinner;
 public class AddRuleActivity extends AppCompatActivity {
     private EditText ruleName;
 
-    private Spinner entrySpinner;
-    private ArrayAdapter<CharSequence> entryAdapter;
-    private Button addNewEntryButton;
-
     private RadioButton radioButtonTextMessage;
     private RadioButton radioButtonAlarm;
 
@@ -31,9 +27,6 @@ public class AddRuleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_rule);
 
         this.ruleName = (EditText) findViewById(R.id.ruleName);
-        // Initiate spinners
-        this.entrySpinner = (Spinner) findViewById(R.id.rule_spinner);
-        this.addNewEntryButton = (Button) findViewById(R.id.add_new_entry_button);
 
         this.radioButtonTextMessage = (RadioButton) findViewById(R.id.radioButtonTextMessage);
         this.radioButtonAlarm = (RadioButton) findViewById(R.id.radioButtonAlarm);
@@ -44,15 +37,6 @@ public class AddRuleActivity extends AppCompatActivity {
         this.numberOfPasses = (EditText) findViewById(R.id.numberOfPasses);
 
         this.saveRuleButton = (Button) findViewById(R.id.save_rule_button);
-
-        // Create ArrayAdapters using the string array and a default spinner layout
-        this.entryAdapter = new ArrayAdapter<CharSequence>(this,android.R.layout.simple_spinner_item);
-
-        // Specify the layout to use when the list of choices appears
-        this.entryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // Apply the adapter to the spinner
-        this.entrySpinner.setAdapter(entryAdapter);
 
         // Set a standard value for number of passes
         this.numberOfPasses.setText("3");
