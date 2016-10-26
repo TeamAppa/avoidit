625f3f3b9109cd4bbadd57dd1fb249bb9f3335fe
Anthon Holmqvist <anthon.kendel@gmail.com>
2016-10-26 16:44:17 -0400

    Changed layout of add rule.

--- android/Avoid.it/app/src/main/java/com/avoidit/AddRuleActivity.java
+++ android/Avoid.it/app/src/main/java/com/avoidit/AddRuleActivity.java
@@ -5,44 +5,54 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 
 public class AddRuleActivity extends AppCompatActivity {
+    private EditText ruleName;
+
     private Spinner entrySpinner;
     private ArrayAdapter<CharSequence> entryAdapter;
-    private Spinner alertSpinner;
-    private ArrayAdapter<CharSequence> alertAdapter;
-    
-    private EditText ruleName;
-    private EditText numberOfPasses;
+    private Button addNewEntryButton;
 
-    private Button addNewEntry;
-    private Button addNewContactButton;
+    private RadioButton radioButtonTextMessage;
+    private RadioButton radioButtonAlarm;
+
+    private EditText contactName;
+    private EditText contactPhonenumber;
+
+    private EditText numberOfPasses;
     private Button saveRuleButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_rule);
+
+        this.ruleName = (EditText) findViewById(R.id.ruleName);
         // Initiate spinners
         this.entrySpinner = (Spinner) findViewById(R.id.rule_spinner);
-        this.alertSpinner = (Spinner) findViewById(R.id.alert_spinner);
-        this.ruleName = (EditText) findViewById(R.id.ruleName);
+        this.addNewEntryButton = (Button) findViewById(R.id.add_new_entry_button);
+
+        this.radioButtonTextMessage = (RadioButton) findViewById(R.id.radioButtonTextMessage);
+        this.radioButtonAlarm = (RadioButton) findViewById(R.id.radioButtonAlarm);
+
+        this.contactName = (EditText) findViewById(R.id.contactName);
+        this.contactName = (EditText) findViewById(R.id.contactPhonenumber);
+
         this.numberOfPasses = (EditText) findViewById(R.id.numberOfPasses);
-        this.addNewEntry = (Button) findViewById(R.id.add_new_entry_button);
+
         this.saveRuleButton = (Button) findViewById(R.id.save_rule_button);
 
         // Create ArrayAdapters using the string array and a default spinner layout
         this.entryAdapter = new ArrayAdapter<CharSequence>(this,android.R.layout.simple_spinner_item);
-        this.alertAdapter = new ArrayAdapter<CharSequence>(this,android.R.layout.simple_spinner_item);
 
         // Specify the layout to use when the list of choices appears
         this.entryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        this.alertAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         // Apply the adapter to the spinner
         this.entrySpinner.setAdapter(entryAdapter);
-        this.alertSpinner.setAdapter(alertAdapter);
 
         // Set a standard value for number of passes
         this.numberOfPasses.setText("3");
